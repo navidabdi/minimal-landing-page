7f56c967ed44b64921b2fed5d59dac5420cf5f6d
navidabdi <artsanaat@gmail.com>
2022-08-28 13:41:21 +0300

    add mobile menu and mobile header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,14 @@
-/*===== MENU =====*/
+/*===== Moblie MENU =====*/
+const triggerMenu = document.querySelector("#trigger-menu"),
+  mobileMenuCon = document.querySelector("#menu-mobile-container"),
+  closeMobileMenu = document.querySelector("#menu-mobile-container .dark-part");
+
+triggerMenu.addEventListener("click", () => {
+  mobileMenuCon.classList.add("active");
+});
+closeMobileMenu.addEventListener("click", () => {
+  mobileMenuCon.classList.remove("active");
+});
 
 /*===== GSAP ANIMATION =====*/
 // NAV
