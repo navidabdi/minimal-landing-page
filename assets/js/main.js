ff54ed1d1dfa96656cf625a88eaa755229901304
navidabdi <artsanaat@gmail.com>
2022-08-29 07:40:11 +0300

    add testimonial carousel section

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,36 @@ closeMobileMenu.addEventListener("click", () => {
   mobileMenuCon.classList.remove("active");
 });
 
+// Testimonial Carousel
+const nextBtn = document.querySelector("#testimonial .next"),
+  pervBtn = document.querySelector("#testimonial .perv"),
+  items = document.querySelectorAll("#testimonial .testimonial-item");
+
+let times = items.length;
+let count = 1;
+
+nextBtn.addEventListener("click", () => {
+  if (count >= times) {
+    count = 1;
+    items[0].style.marginLeft = "0";
+  } else {
+    items[0].style.marginLeft = `-${items[0].offsetWidth * count}px`;
+    count++;
+  }
+});
+
+pervBtn.addEventListener("click", () => {
+  if (count === 1) {
+    items[0].style.marginLeft = `-${items[0].offsetWidth * (times - 1)}px`;
+    count = times;
+  } else {
+    items[0].style.marginLeft = `-${
+      items[0].offsetWidth * (count - 1) - items[0].offsetWidth
+    }px`;
+    count--;
+  }
+});
+
 /*===== GSAP ANIMATION =====*/
 // NAV
 
